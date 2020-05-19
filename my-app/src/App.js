1ef1e4b2a5462d2b8ed8bd8f38f6412aa5b13794
James-Hardy <jamesjhardy95@gmail.com>
2020-05-19 08:53:51 +0100

    remove old file and update with React app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        {/* Nav Bar */}
+        {/* Hero Image and title*/}
+      </header>
+    </>
+  )
+}
+
+export default App
