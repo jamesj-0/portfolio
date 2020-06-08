9847b5871160e525f73a1f9f559fbca95b53a473
James-Hardy <jamesjhardy95@gmail.com>
2020-06-08 22:42:58 +0100

    add flex to home page

--- portfolio-site/src/components/home.js
+++ portfolio-site/src/components/home.js
@@ -6,6 +6,7 @@ import {
   Text1,
   Title2,
   Title3,
+  Container,
 } from '../styles/home';
 import headShotImage from './Assets/16200025.JPG';
 
@@ -13,14 +14,18 @@ export default function Home() {
   return (
     <BGContainer>
       <Title1>My Portfolio</Title1>
-      <Text1>
-        I’m a Full Stack Developer with a background in Music and Teaching. I am
-        passionate about social impact projects and working with new
-        technologies. I love electronic music, cooking and cats.
-      </Text1>
-      <LandingImg src={headShotImage} alt="Image of James" />
-      <Title2>Full Stack</Title2>
-      <Title3>Developer</Title3>
+      <Container>
+        <Text1>
+          I’m a Full Stack Developer with a background in Music and Teaching. I
+          am passionate about social impact projects and working with new
+          technologies. I love electronic music, cooking and cats.
+        </Text1>
+        <LandingImg src={headShotImage} alt="Image of James" />
+      </Container>
+      <Container>
+        <Title2>Full Stack</Title2>
+        <Title3>Developer</Title3>
+      </Container>
     </BGContainer>
   );
 }
--- portfolio-site/src/styles/home.js
+++ portfolio-site/src/styles/home.js
@@ -1,46 +1,38 @@
 import styled from 'styled-components';
 
 const BGContainer = styled.body`
-  position: relative;
-  width: 1440px;
-  height: 1024px;
+  width: 100vw;
+  height: 100vh;
   background: #db8452;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Container = styled.section`
+  display: flex;
+  flex-direction: row;
+  // margin: 0 10rem 0rem 10rem;
 `;
 
 const Title1 = styled.h1`
-  position: absolute;
-  width: 362px;
-  height: 70px;
-  left: 166px;
-  top: 266px;
-
   font-family: Canela;
   font-style: normal;
   font-weight: normal;
   font-size: 70px;
   line-height: 70px;
-
+  margin-right: 12rem;
   display: flex;
-  align-items: center;
   text-transform: capitalize;
-
   color: #ffffff;
 `;
 
 const Title2 = styled.h1`
-  position: absolute;
-  width: 278px;
-  height: 70px;
-  left: 999px;
-  top: 761px;
-
   font-family: Canela;
   font-style: normal;
   font-weight: normal;
   font-size: 70px;
   line-height: 70px;
-  /* identical to box height */
-
   display: flex;
   align-items: center;
   text-transform: capitalize;
@@ -49,19 +41,11 @@ const Title2 = styled.h1`
 `;
 
 const Title3 = styled.h1`
-  position: absolute;
-  width: 254px;
-  height: 87px;
-  left: 999px;
-  top: 811px;
-
   font-family: JostLight;
   font-style: normal;
   font-weight: 300;
   font-size: 60px;
   line-height: 87px;
-  /* identical to box height */
-
   display: flex;
   align-items: center;
   text-align: center;
@@ -70,31 +54,24 @@ const Title3 = styled.h1`
 `;
 
 const Text1 = styled.p`
-  position: absolute;
-  width: 231px;
-  height: 261px;
-  left: 291px;
-  top: 364px;
-
   font-family: JostBook;
   font-style: normal;
   font-weight: normal;
+  width: 500px;
   font-size: 20px;
   line-height: 29px;
   text-align: right;
-
+  margin: 0 1rem 0 0;
   color: #ffffff;
 `;
 
 //FIX MEDIA QUERIES
 const LandingImg = styled.img`
-  position: absolute;
   width: 365px;
   height: 453px;
-  left: 539px;
-  top: 370px;
+  margin-right: 30%;
   object-fit: cover;
   object-position: 40% 0;
 `;
 
-export { Title1, Title2, Title3, Text1, LandingImg, BGContainer };
+export { Title1, Title2, Title3, Text1, LandingImg, BGContainer, Container };
